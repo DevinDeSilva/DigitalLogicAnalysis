4c88a3d3e44707027ccbba1961349e99344705b4
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2021-08-29 18:44:50 +0530

    unit test for max term class

--- tests/inputs/test_max_terms.py
+++ tests/inputs/test_max_terms.py
@@ -18,7 +18,31 @@ class InputModuleTestCase(unittest.TestCase):
         self.assertEqual(a.get_terms(), None)
 
         a.set_terms([0, 2])
-        self.assertEqual(a.get_terms(), [0, 2])  # add assertion here
+        self.assertEqual(a.get_terms(), [0, 2])
+
+    def test_validation_str_array(self):
+        a = MaxTerms(2)
+        try:
+            a.set_terms([0, '1'])
+            self.assertEqual(False, True)
+        except ValueError as e:
+            self.assertEqual(str(e), "List can only contain Integers")
+
+    def test_validation_max_value(self):
+        a = MaxTerms(2)
+        try:
+            a.set_terms([0, 2, 6, 7])
+            self.assertEqual(False, True)
+        except ValueError as e:
+            self.assertEqual(str(e), "All values in list must be lower than 2**num_variables and non negative")
+
+    def test_validation_num_elements(self):
+        a = MaxTerms(2)
+        try:
+            a.set_terms([0, 1, 2, 1, 2, 2, 2])
+            self.assertEqual(a.get_terms(), [0, 1, 2])
+        except ValueError as e:
+            self.assertEqual(False, True)
 
 
 if __name__ == '__main__':
